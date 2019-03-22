f8c9c10c610e078f292a3787bea9b67e86b53a7d
sinouye <sean.inouye@wsu.edu>
2019-03-21 23:55:42 -0700

    added hours/categories

--- sample.py
+++ sample.py
@@ -78,5 +78,83 @@ def insertUser():
     print(count_line)
     f.close()
 
+def insertCategories():
+    with open('C:/users/sinou/Desktop/yelp_business.json', 'r') as f:
+        line = f.readline()
+        count_line = 0
+
+        try:
+            conn = psycopg2.connect("dbname='milestone2' user='postgres' host='localhost' password='pass'")
+
+        except:
+            print("Error: cannot connect")
+        cur = conn.cursor()
+
+        while line:
+            data = json.loads(line)
+            
+            categ = data['categories']
+            for cat in categ:
+                sql_str = "INSERT INTO categories(business_id, category_name) " + \
+                   "VALUES ('" + cleanStr4SQL(data['business_id']) + "','" + cleanStr4SQL(cat) + "');"
+                try:
+                    cur.execute(sql_str)
+                except psycopg2.Error as e: 
+                    print("Insert to user table failed...")
+                    print("Error: ", e)
+                conn.commit()
+            # optionally you might write the INSERT statement to a file.
+            # outfile.write(sql_str)
+
+            line = f.readline()
+            count_line +=1
+
+        cur.close()
+        conn.close()
+
+    print(count_line)
+    #outfile.close()  #uncomment this line if you are writing the INSERT statements to an output file.
+    f.close()
+
+def insertHours():
+    with open('C:/users/sinou/Desktop/yelp_business.json', 'r') as f:
+        line = f.readline()
+        count_line = 0
+
+        try:
+            conn = psycopg2.connect("dbname='milestone2' user='postgres' host='localhost' password='pass'")
+
+        except:
+            print("Error: cannot connect")
+        cur = conn.cursor()
+
+        while line:
+            data = json.loads(line)
+            
+            hours = data['hours']
+            for days in hours:
+                sql_str = "INSERT INTO hours (business_id, day, open, close) VALUES ('" + \
+                    cleanStr4SQL(data['business_id']) + "','" + days + "','" + \
+                        hours[days].split("-")[0] + "','" + hours[days].split("-")[1] + "');"
+
+                try:
+                    cur.execute(sql_str)
+                except psycopg2.Error as e: 
+                    print("Insert to user table failed...")
+                    print("Error: ", e)
+                conn.commit()
+            # optionally you might write the INSERT statement to a file.
+            # outfile.write(sql_str)
+
+            line = f.readline()
+            count_line +=1
+
+        cur.close()
+        conn.close()
+
+    print(count_line)
+    #outfile.close()  #uncomment this line if you are writing the INSERT statements to an output file.
+    f.close()
+
 
 insert2BusinessTable()
