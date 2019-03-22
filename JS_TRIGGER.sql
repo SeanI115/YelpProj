4541e7bdbd66f9a6c235301183750694720553fd
jhennafc <jfcaldetera@gmail.com>
2019-03-22 11:07:00 -0700

    TRIGGERS AND UPDATESSSS

--- JS_TRIGGER.sql
+++ JS_TRIGGER.sql
@@ -13,3 +13,23 @@ CREATE TRIGGER UpdateReviewRating
 		SET avg_rev = 
 			SUM(SELECT stars FROM reviews WHERE reviews.busines_id = business_id) /
 			COUNT (SELECT * FROM reviews WHERE reviews.business_id = business_id);		
+			
+UPDATE review
+		SET num_revs = COUNT (SELECT review_id FROM reviews WHERE reviews.business_id = business_id);
+		
+UPDATE review
+		SET avg_rev = 
+			SUM(SELECT stars FROM reviews WHERE reviews.business_id = business_id) /
+			COUNT (SELECT * FROM reviews WHERE reviews.business_id = business_id);		
+			
+CREATE TRIGGER CheckinCountIncrement
+	AFTER INSERT ON checkins
+	FOR EACH ROW 
+		SET checkins.count = checkins.count + 1;
+		
+CREATE TRIGGER CheckinCountDecrement
+	AFTER DELETE ON reviews
+	FOR EACH ROW
+		SET checkins.count = checkins.count - 1;
+		
+	
